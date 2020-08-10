91e4e9f147e2d6b92459771da6faf5f6869f9a80
SurendraSapkale <chf.suri@gmail.com>
2020-08-10 16:45:59 +1000

    set up order fuction

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,9 @@
 class OrderController < ApplicationController
   def page
     # create an entry in buyres table 
+    @buyer = Buyer.new
+    @buyer.profile_id = current_user.profile.id 
+    @buyer.save
+    
   end
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,5 @@ class Dish < ApplicationRecord
   belongs_to :seller
   belongs_to :chef_profile
   has_one_attached :picture
+  has_many :orders
 end
